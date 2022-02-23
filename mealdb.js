845839130993e021b33a04f58e4fe8e68bdd9077
coderowson <runurowson@gmail.com>
2022-02-23 20:42:30 +0600

    after grid

--- mealdb.js
+++ mealdb.js
@@ -14,19 +14,22 @@ const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
         console.log(meal);
-        const div = document.createElement('div')
+        const div = document.createElement('div');
         div.classList.add('col')
-        div.innerHtml = `
-        <div class="card">
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top w-50" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                <h5 class="card-title">${meal.idMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 30)}This is a longer card with supporting text below as a natural lead-in to
                     additional content. This content is a little bit longer.</p>
             </div>
         </div>
         `;
         searchResult.appendChild(div);
-
     })
+}
+
+const loadMealDetail = mealId => {
+    console.log(mealId)
 }
